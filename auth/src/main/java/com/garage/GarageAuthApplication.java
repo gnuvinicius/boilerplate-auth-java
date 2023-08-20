d1381db09e1a700a0788c8074e19d92c94f3aaf3
Vinicius Claudino <gnu.vinicius@gmail.com>
2023-08-20 10:50:46 -0300

    funcionando!

--- auth/src/main/java/com/garage/GarageAuthApplication.java
+++ auth/src/main/java/com/garage/GarageAuthApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.garage.auth")
+@ComponentScan("com.garage")
 public class GarageAuthApplication {
 
 	public static void main(String[] args) {
